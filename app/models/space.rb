1fb02b65f4b157632700f3ee0142b51968834673
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2021-02-21 09:40:27 -0500

    Add support for geocoding.

--- app/models/space.rb
+++ app/models/space.rb
@@ -18,6 +18,13 @@ class Space < ApplicationRecord
 
   before_save :find_languages, :find_indicators
 
+  after_validation :geocode
+  geocoded_by :full_address
+
+  def full_address
+    [self.address.address_1, self.address.address_2, self.address.city, self.address.postal_code, self.address.country].compact.join(",")
+  end
+
   private
 
   def find_languages
@@ -31,4 +38,5 @@ class Space < ApplicationRecord
       Indicator.find_by(name: indicator.name)
     end
   end
+
 end
