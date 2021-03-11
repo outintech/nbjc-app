64c04ba77351135063e52e8606e9908c23144c8b
Manu Gomez <mgomez@digitalocean.com>
2021-03-10 19:15:45 -0500

    adds profile info to users table

---
+++ db/migrate/20210309223147_add_profile_info.rb
@@ -0,0 +1,16 @@
+class AddProfileInfo < ActiveRecord::Migration[6.0]
+  def up
+    add_column :users, :auth0_id, :string
+    add_index :users, :auth0_id
+    change_column_null :users, :auth0_id, false
+
+    add_column :users, :pronouns, :string
+    add_column :users, :location, :string
+  end
+
+  def down
+    remove_column :users, :auth0_id, :string
+    remove_column :users, :pronouns, :string
+    remove_column :users, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_020052) do
+ActiveRecord::Schema.define(version: 2021_03_09_223147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,10 @@ ActiveRecord::Schema.define(version: 2021_03_04_020052) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "auth0_id", null: false
+    t.string "pronouns"
+    t.string "location"
+    t.index ["auth0_id"], name: "index_users_on_auth0_id"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
