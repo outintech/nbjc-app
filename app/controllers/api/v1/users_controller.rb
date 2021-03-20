adcce3de32785b06ce813a39dec13efb66864046
Manu Gomez <mgomez@digitalocean.com>
2021-03-20 10:05:58 -0400

    adds test for a single user profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,38 +21,42 @@ class Api::V1::UsersController < ApplicationController
   # GET /users/:id
   def show
     # As our current profiles go, you can't ask for information about another user
-    verify_user
-    # sparse fields
-    @include = []
-    @fields = params[:fields].split(',') unless params[:fields].nil? || params[:fields].blank?
-    # todo: validate includable fields?
-    @include = params[:include].split(',') unless params[:include].nil? || params[:include].blank?
+    if verify_user
+      # sparse fields
+      @include = []
+      @fields = params[:fields].split(',') unless params[:fields].nil? || params[:fields].blank?
+      # todo: validate includable fields?
+      @include = params[:include].split(',') unless params[:include].nil? || params[:include].blank?
 
-    if @fields.length > 0
-      @user = User.where(id: params[:id]).select(@fields)
+      if @fields && @fields.length > 0
+        @user = User.where(id: params[:id]).select(@fields)
+      else
+        find_user
+      end
+      if @user 
+        render json: { data: { user: @user } }, status: 200, include: @include
+      end
     else
-      find_user
+      render json: { error: 'Forbidden' }, status: 403
     end
-
-    render json: { data: { user: @user } }, status: 200, include: @include
   end
 
   def create
     # check that the provided :auth0_id corresponds to the token auth0_id
     if auth0_id != @current_user.auth0_id
       render json: { error: 'Forbidden' }, status: 403
-    end
-    @user = User.new(user_params)
-    if @user.save!
-      render json: { data: { user: @user } }, status: 201
-    else
-      render json: { error: 'Unable to save user' }, status: 400
+    else 
+      @user = User.new(user_params)
+      if @user.save!
+        render json: { data: { user: @user } }, status: 201
+      else
+        render json: { error: 'Unable to save user' }, status: 400
+      end
     end
   end
 
   def update
-    verify_user
-    if @current_user.update!(user_params)
+    if verify_user && @current_user.update!(user_params)
       render :show
     else
       render json: { errors: @current_user.errors }, status: :unprocessable_entity
@@ -83,13 +87,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def verify_user
-    if params[:id] != @current_user.id
-      render json: { error: 'Forbidden' }, status: 403
-    end
-  end
-
-  def current_user
-    # TODO figure out how to get current user
-    current_user = get_current_user!
+    params[:id] != @current_user.id
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -14,8 +14,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     describe "With a valid auth token" do
-      it 'gets returns an error if user is not found' do
+      before do 
         controller.stub(:authenticate_request! => true)
+      end
+      
+      it 'gets returns an error if user is not found' do
         controller.stub(:get_current_user! => nil)
         newUser = "provider|5678"
         get :index, params: { auth0_id: newUser }
@@ -23,16 +26,25 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(JSON.parse(response.body)["error"]).to eq("User not found")
       end
 
-      it 'gets a user id given an auth0 id with valid authorization' do
-        controller.stub(:authenticate_request! => true)
+      it 'gets a user id given an auth0 id' do
         @auth0_id = "provider|1234"
         payload = { sub: @auth0_id }
         token = JWT.encode payload, nil, 'none'
-        puts "TOKEN #{token}"
         request.headers["Authorization"] = "Bearer #{token}"
         get :index, params: { auth0_id: @auth0_id }
         expect(response.status).to eq(200)
         expect(JSON.parse(response.body)['data']['user']['user_id']).to eq(user.id)
+      end
+
+      it 'gets a user\'s profile' do
+        @auth0_id = "provider|1234"
+        payload = { sub: @auth0_id }
+        token = JWT.encode payload, nil, 'none'
+        request.headers["Authorization"] = "Bearer #{token}"
+        get :show, params: { id: 4 }
+        expect(response.status).to eq(200)
+        expect(JSON.parse(response.body)['data']['user']['auth0_id']).to eq(@auth0_id)
+        expect(JSON.parse(response.body)['data']['user']['username']).to eq(user.username)
       end
     end
   end
