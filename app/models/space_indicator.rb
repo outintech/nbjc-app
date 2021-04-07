c5de207d567e551e4ca7ac951e76042a953bdd76
afhare <47701073+afhare@users.noreply.github.com>
2021-04-06 19:22:37 -0600

    Indicators are fully hooked up when creating a space. Hours of operation imported from Yelp - minor tweak. (#27)

--- app/models/space_indicator.rb
+++ app/models/space_indicator.rb
@@ -1,4 +1,23 @@
 class SpaceIndicator < ApplicationRecord
   belongs_to :space
   belongs_to :indicator
+
+  def self.create_indicators_for_space(space_indicators, space)
+    indicators = []
+    space_indicators.each do |indicator|
+      begin
+        db_indicator = Indicator.find_by(name: indicator["name"])
+        space_indicator = SpaceIndicator.new(indicator: db_indicator, space: space)
+        indicators << space_indicator
+      rescue
+      end
+    end
+    indicators
+  end
+
+  def self.save_indicators(space_indicators)
+    space_indicators.each do |indicator|
+      indicator.save
+    end
+  end
 end
