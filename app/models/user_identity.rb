cac70ccca60caa0e000483e35371515be75e7c1b
Deepak Venkatachalam <dvenkatachalam@atlassian.com>
2021-04-07 18:33:12 -0400

    Safe guard against nil identities.

--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -4,7 +4,7 @@ class UserIdentity < ApplicationRecord
   
   def self.create_identities_for_user(user_identities, user)
     identities = []
-    user_identities.each do |identity|
+    (user_identities || []).each do |identity|
       begin
         db_identity = Identity.find_by(name: identity["name"])
         user_identity = UserIdentity.new(identity: db_identity, user: user)
@@ -16,7 +16,7 @@ class UserIdentity < ApplicationRecord
   end
 
   def self.save_identities(user_identities)
-    user_identities.each do |identity|
+    (user_identities || []).each do |identity|
       identity.save
     end
   end 
