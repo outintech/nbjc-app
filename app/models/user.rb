ca048b25513214a6013fa213e2450ad5f71a00fe
afhare <47701073+afhare@users.noreply.github.com>
2021-05-16 18:37:02 -0600

    Username validations on backend: brought formatting back into validation, and updated FactoryBot for username generation. Also added method to be used in the future to ensure username is unique on field form entry. (#30)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :user_identities, dependent: :destroy
   has_many :identities, :through => :user_identities
-  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false # format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
   # allows '.' and ''' in the name for e.g. Kandis O'Connell or Errol Rutherford Jr.
   validates :name, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z\s\.\']+\z/ }
   accepts_nested_attributes_for :identities
   before_save :find_identities
+
+  def self.username_unique_amongst_members(username)
+    User.where('username is not null and username != ?', '').where(username: username).count == 0
+  end
 
   private
   def find_identities
